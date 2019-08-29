151de50dea50d8ba6162fa0320e321552b50e80a
Cristian <Cristian@10.90.4.237>
2019-08-29 15:34:42 +0200

    closes #1 - project setup

---
+++ src/test/java/com/sirctech/MsscBreweryServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package com.sirctech;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryServiceApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
