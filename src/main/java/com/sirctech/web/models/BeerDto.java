ec2c72460da2c089facf4d186d98dc1f70c753ee
Cristian <Cristian@10.90.4.237>
2019-08-29 15:39:25 +0200

    closes #2 - model

---
+++ src/main/java/com/sirctech/web/models/BeerDto.java
@@ -0,0 +1,20 @@
+package com.sirctech.web.models;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+	private UUID id;
+	private String name;
+	private String style;
+	private Long upc;
+	
+}
